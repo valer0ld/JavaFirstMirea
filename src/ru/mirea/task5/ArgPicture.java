848cd15c66e9ee7c9a4cc8b64b67ea3d9e2b57d4
Валера <valeronpe@mail.ru>
2022-10-30 21:55:56 +0300

    5 task

---
+++ src/ru/mirea/task5/ArgPicture.java
@@ -0,0 +1,34 @@
+package ru.mirea.task5;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
+
+//java ArgPicture.java C:\Users\Валера\Pictures\photo5294371182251981282.jpg
+public class ArgPicture extends JFrame {
+
+    public ArgPicture(String path){
+        super("RandomFigure");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 500);
+
+
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(0, 0, 0));
+        panel.setPreferredSize(new Dimension(500,  500));
+
+        ImageIcon icon1 = new ImageIcon(path);
+        JLabel label =  new JLabel(icon1);
+
+        panel.add(label);
+
+        getContentPane().add(panel);
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ArgPicture(args[0]);
+    }
+}
