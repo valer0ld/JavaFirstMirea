e6bbb8a06b4961fdd8f60600b9b48fb992415941
Валера <valeronpe@mail.ru>
2022-11-29 01:56:19 +0300

    15 task

---
+++ src/ru/mirea/task15/ex1/SimpleCalculator.java
@@ -0,0 +1,90 @@
+package ru.mirea.task15.ex1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Flow;
+
+public class SimpleCalculator extends JFrame {
+    JTextField jta1 = new JTextField(12);
+    JTextField jta2 = new JTextField(12);
+    JButton add = new JButton("+");
+    JButton sub = new JButton("-");
+    JButton mul = new JButton("*");
+    JButton div = new JButton("/");
+
+    private static String op;
+
+    SimpleCalculator() {
+        super("Simple Calculator");
+        setLayout(new FlowLayout());
+        setSize(250,150);
+
+        add(new JLabel("1st Number", JLabel.CENTER));
+        add(jta1);
+        add(new JLabel("2nd Number", JLabel.CENTER));
+        add(jta2);
+        add(add);
+        add(sub);
+        add(mul);
+        add(div);
+
+        ActionListener doOperation = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                try{
+                    double x1 = Double.parseDouble(jta1.getText().trim());
+                    double x2 = Double.parseDouble(jta2.getText().trim());
+                    JOptionPane.showMessageDialog(null,"Result = "+ result(x1, x2),"RESULT",JOptionPane.INFORMATION_MESSAGE);
+                }catch(Exception e)
+                {
+                    JOptionPane.showMessageDialog(null,   "Error in Numbers!","ALERT" ,JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        };
+
+        add.addActionListener(doOperation);
+        sub.addActionListener(doOperation);
+        mul.addActionListener(doOperation);
+        div.addActionListener(doOperation);
+
+        add.addActionListener(new ActionListener(){
+             public void actionPerformed(ActionEvent ae)
+        {
+            op = "+";
+        }
+        });
+        sub.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae)
+            {
+                op = "-";
+            }
+        });
+        mul.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae)
+            {
+                op = "*";
+            }
+        });
+        div.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae)
+            {
+                op = "/";
+            }
+        });
+
+        setVisible(true);
+    }
+    public static double result(double x1, double x2)
+    {
+        return switch (op) {
+            case "+" -> x1 + x2;
+            case "-" -> x1 - x2;
+            case "*" -> x1 * x2;
+            case "/" -> x1 / x2;
+            default -> 0;
+        };
+    }
+    public static void main(String[]args){ new SimpleCalculator();}
+}
---
+++ src/ru/mirea/task15/ex4/Calculator.java
@@ -0,0 +1,120 @@
+package ru.mirea.task15.ex4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculator extends JFrame {
+
+    JPanel[] jPanel = new JPanel[2];
+
+    String[] buttonsText = {"7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"};
+
+    JTextArea textarea = new JTextArea(6, 14);
+    Dimension buttonSize = new Dimension(65, 24);
+
+    Font font = new Font("",Font.PLAIN,15);
+
+
+    Calculator() {
+        super("Simple Calculator");
+        setLayout(new GridLayout(2, 1));
+        jPanel[0] = new JPanel();
+        jPanel[1] = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 20));
+        add(jPanel[0]);
+        add(jPanel[1]);
+
+        setSize(350, 480);
+
+        for (int i = 0; i < 16; i++) {
+            Button button = new Button(buttonsText[i]);
+            button.setBackground(new Color(255, 255, 255));
+            button.setPreferredSize(buttonSize);
+            button.setFont(font);
+            if (!buttonsText[i].matches("=")) {
+                button.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent ae) {
+                        textarea.append(button.getLabel());
+                    }
+                });
+            }
+            else button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent ae) {
+                    result(textarea.getText());
+                }
+            });
+            jPanel[1].add(button);
+        }
+
+        textarea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        textarea.setFont(new Font("", Font.BOLD, 25));
+        textarea.setLineWrap(true);
+        jPanel[0].add(textarea);
+
+        setVisible(true);
+    }
+
+    public static void result(String expression)
+    {
+        StringBuilder exp = new StringBuilder(expression);
+        while(Pattern.compile("[\\*\\/]").matcher(exp).find())
+        {
+            Pattern fullExp = Pattern.compile("(\\d+\\.?\\d*[\\*\\/]\\d+\\.?\\d*)");
+            Matcher m = fullExp.matcher(exp);
+            Matcher m1 = Pattern.compile("[\\*\\/]").matcher(exp);
+           // System.out.println(fullExp.matcher(exp));
+            int start = m.find() ? m.start() : 0;
+            int end = m.end();
+            int op = m1.find() ? m1.start() : 0;
+            //System.out.println(exp.substring(start, op) + " " + exp.substring(op + 1, end));
+            double x1 = Double.parseDouble(exp.substring(start, op));
+            double x2 = Double.parseDouble(exp.substring(op + 1, end));
+            //System.out.println(x1 + " " + x2);
+            if (m1.group().equals("*"))
+            {
+                exp.delete(start, end);
+                exp.insert(start, (x1 * x2));
+            }
+            else
+            {
+                exp.delete(start, end);
+                exp.insert(start, (x1 / x2));
+            }
+        }
+        while(Pattern.compile("[\\+\\-]").matcher(exp).find())
+        {
+            Pattern fullExp = Pattern.compile("(\\d+\\.?\\d*[\\+\\-]\\d+\\.?\\d*)");
+            Matcher m = fullExp.matcher(exp);
+            Matcher m1 = Pattern.compile("[\\+\\-]").matcher(exp);
+            // System.out.println(fullExp.matcher(exp));
+            int start = m.find() ? m.start() : 0;
+            int end = m.end();
+            int op = m1.find() ? m1.start() : 0;
+            //System.out.println(exp.substring(start, op) + " " + exp.substring(op + 1, end));
+            double x1 = Double.parseDouble(exp.substring(start, op));
+            double x2 = Double.parseDouble(exp.substring(op + 1, end));
+            //System.out.println(x1 + " " + x2);
+            if (m1.group().equals("+"))
+            {
+                exp.delete(start, end);
+                exp.insert(start, (x1 + x2));
+            }
+            else
+            {
+                exp.delete(start, end);
+                exp.insert(start, (x1 - x2));
+            }
+        }
+        try {
+            JOptionPane.showMessageDialog(null, "Result = " + Math.ceil(Double.parseDouble(exp.toString()) * 1000)/ 1000, "RESULT", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Wrong expression!", "ALERT", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static void main(String[]args){ new Calculator();}
+}
